5a5acac318289e1e9ff0b139e35a718de6cb2a13
natalieybl <94332856+natalieybl@users.noreply.github.com>
2024-08-25 18:40:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,5 @@ app=Flask(__name__)
 def index():
   return render_template('index.html')
 
-@app.route('/', methods=["get","post"])
-def dapp2():
-  return render_template('dapp2.html')
-
-  if __name__=='__main__':
+if __name__=='__main__':
     app.run()
